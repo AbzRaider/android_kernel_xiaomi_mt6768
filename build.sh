9d1c495eadc2587282288df6d7421588f1715226
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-04-24 05:34:42 +0000

    buildscript : Switch to Azure  Clang 15

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ ccache -M 100G
 export ARCH=arm64
 export KBUILD_BUILD_HOST=MARKxDEVS
 export KBUILD_BUILD_USER="AbzRaider"
-git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
-git clone --depth=1 https://github.com/sarthakroy2002/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7 los-4.9-64
-git clone --depth=1 https://github.com/sarthakroy2002/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
+git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git  clang
+
+
 
  if ! [ -d "out" ]; then
 echo "Kernel OUT Directory Not Found . Making Again"
@@ -20,18 +20,20 @@ fi
 
 make O=out ARCH=arm64 lancelot_defconfig
 
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC="clang" \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-gnu-" \
-                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-gnueabihf-" \
-                      LD=ld.lld \
+                      CROSS_COMPILE="${PWD}/clang/bin/aarch64-linux-gnu-" \
+                      CROSS_COMPILE_ARM32="${PWD}/clang/bin/arm-linux-gnueabi-" \
+		      LD=ld.lld \
+
                       AS=llvm-as \
 		              AR=llvm-ar \
 			          NM=llvm-nm \
 			          OBJCOPY=llvm-objcopy \
+				  OBJDUMP=llvm-objdump \
                       CONFIG_NO_ERROR_ON_MISMATCH=y
 }
 
